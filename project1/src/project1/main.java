e8f0de40553561562ce510e67e46ad51a954ae74
thai <bin23102008phu@gmail.com>
2026-09-20 00:03:52 +0700

    them gitignore, add project1

---
+++ project1/src/project1/main.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class main {
+	public static void main(String[] args) {
+		// đây là program đầu tiên 
+		System.out.println("hello");
+		System.out.print("okok\n");
+		System.out.print("okok\n");
+	}
+
+}
